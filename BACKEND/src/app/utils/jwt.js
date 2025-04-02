52505c4e3c50ba69445561e0d4b9f0c6801f008a
Luana Manuelle <luanamanuelle63@gmail.com>
2025-04-02 19:43:29 -0300

    Atualização completa do backend(API) e frontend(SPA)

--- BACKEND/src/app/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import authConfig from "../config/auth.js";
-
-const SECRET = process.env.JWT_SECRET_KEY;
-
-export const generateToken = (payload) => {
-    return jwt.sign(payload, SECRET, { expiresIn: authConfig.expiresIn });
-};
